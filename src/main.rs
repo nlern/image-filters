239a85df9d89b06d1aa11773b9bf868e989d3e6c
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-18 21:08:09 +0530

    feat: added input, output textures

--- src/main.rs
+++ src/main.rs
@@ -18,4 +18,43 @@ fn main() -> anyhow::Result<()> {
     // load image
     let input_image = image::load_from_memory(include_bytes!("sushi.png"))?.to_rgba8();
     let (width, height) = input_image.dimensions();
+
+    let texture_size = wgpu::Extent3d {
+        width,
+        height,
+        depth_or_array_layers: 1,
+    };
+
+    let input_texture = device.create_texture(&wgpu::TextureDescriptor {
+        label: Some("input texture"),
+        size: texture_size,
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Rgba8Unorm,
+        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+    });
+
+    queue.write_texture(
+        input_texture.as_image_copy(),
+        bytemuck::cast_slice(input_image.as_raw()),
+        wgpu::ImageDataLayout {
+            offset: 0,
+            bytes_per_row: std::num::NonZeroU32::new(4 * width),
+            rows_per_image: None, // doesn't need to be specified as we're writing a single image
+        },
+        texture_size,
+    );
+
+    let output_texture = device.create_texture(&wgpu::TextureDescriptor {
+        label: Some("output texture"),
+        size: texture_size,
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Rgba8Unorm,
+        usage: wgpu::TextureUsages::COPY_SRC | wgpu::TextureUsages::STORAGE_BINDING,
+    });
+
+    
 }
