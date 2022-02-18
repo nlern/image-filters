f475e16228c2f7ab5425400a9abc67baeb067548
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-18 20:41:36 +0530

    feat: created device and queue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    use pollster::FutureExt;
+
+    let instance = wgpu::Instance::new(wgpu::Backends::all());
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: None,
+        })
+        .block_on()
+        .ok_or(anyhow::anyhow!("Couldn't create the adapter"))?;
+    let (device, queue) = adapter
+        .request_device(&Default::default(), None)
+        .block_on()?;
+
+    
 }
