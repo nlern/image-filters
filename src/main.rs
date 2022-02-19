e3fbaf28a6aad8faceaca2ba5bb81828708aa84d
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-19 19:30:45 +0530

    feat: count work group, create encoder

--- src/main.rs
+++ src/main.rs
@@ -86,4 +86,17 @@ fn main() -> anyhow::Result<()> {
             },
         },
     });
+
+    let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+}
+
+fn compute_work_group_count(
+    (width, height): (u32, u32),
+    (workgroup_width, workgroup_height): (u32, u32),
+) -> (u32, u32) {
+    let x = (width + workgroup_width - 1) / workgroup_width;
+    let y = (height + workgroup_height - 1) / workgroup_height;
+
+    (x, y)
 }
